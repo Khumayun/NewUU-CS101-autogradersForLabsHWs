ba01bcbccac6aaf8f02219db3a1184f460714ff2
Khumoyun Aminaddinov <k.aminaddinov@innopolis.university>
2024-03-12 00:55:58 +0500

    fixed setup step by removing source folder

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ echo "Installing apt dependencies..."
 apt-get install -y python3 python3-pip python3-dev
 
 echo "Installing python3 dependencies..."
-pip3 install -r /autograder/source/requirements.txt
+pip3 install -r /autograder/requirements.txt
 
 echo "Installing Gradescope dependencies..."
-cd /autograder/source/GradescopeBase-master
+cd /autograder/GradescopeBase-master
 python3 setup.py install
 cd ../
