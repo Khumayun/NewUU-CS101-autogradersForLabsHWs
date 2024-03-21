f1d82d12623000ea621233c143e527e6d8371d7e
k.aminaddinov <k.aminaddinov@newuu.uz>
2024-03-21 16:03:14 +0500

    updated tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,155 +3,134 @@ import subprocess
 import re
 
 from GradescopeBase import Autograder, AutograderTest, Test
-from GradescopeBase.Utils import root_dir
 
 PROBLEM_NO = os.path.basename(__file__)
 PROBLEM_NO = PROBLEM_NO.split('_')[1].split('.')[0][1:]
 
 INPUTS_FOR_CIN = [
-	"29.9",
-	"328", "23", "92",
-	"F", "1.65",
-	"18:20:3F:20:AB:11",
-	"5", "+", "42"
+    "29.9",
+    "328", "23", "92",
+    "F", "1.65",
+    "18:20:3F:20:AB:11",
+    "5", "+", "42"
 ]
+# STUDENT_SUBMISSION = f"/autograder/submission/main.cpp"
+# TRUE_RESULT = f"/autograder/source/tests/solutions/truemain.cpp"
 
-STUDENT_SUBMISSION = f"{root_dir()}/submission/main.cpp"
-TRUE_RESULT = f"{root_dir()}/tests/solutions/truemain.cpp"
+# for local run
+STUDENT_SUBMISSION = f"submission/main.cpp"
+TRUE_RESULT = f"tests/solutions/truemain.cpp"
 
 COMPILED_STUDENT_SUBMISSION = os.system(
-	f"g++ -c {STUDENT_SUBMISSION} && g++ -o main main.o")
+    f"g++ -c {STUDENT_SUBMISSION} && g++ -o main main.o")
 
 COMPILED_TRUE_RESULT = os.system(
-	f"g++ -c {TRUE_RESULT} && g++ -o truemain truemain.o")
+    f"g++ -c {TRUE_RESULT} && g++ -o truemain truemain.o")
 
 
-def compile_solutions():
-	process = subprocess.Popen([f"./main"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	_output_student, _error_student = process.communicate(input=' '.join(INPUTS_FOR_CIN).encode('utf-8'))
+def applyPattern(full_text, solution_words, no):
+    full_text = full_text[no].strip()
+    solution_words = solution_words[no].strip()
 
-	process = subprocess.Popen([f"./truemain"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	_output_true, _error_true = process.communicate(input=' '.join(INPUTS_FOR_CIN).encode('utf-8'))
-	return _output_true, _output_student
+    pattern = r'\b'
+    for word in solution_words:
+        pattern += word
+        pattern += '\b\W+(?:\w+\W+)*'
+    pattern += '\b'
+
+    return re.search(pattern, full_text) and full_text != ['']
 
 
-output_true, output_student = compile_solutions()
+def compileCPP():
+    process = subprocess.Popen([f"./main"], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, shell=True)
+    output_student, error_student = process.communicate(input=' '.join(INPUTS_FOR_CIN).
+                                                        encode('utf-8'))
+
+    process = subprocess.Popen([f"./truemain"], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, shell=True)
+    output_true, error_true = process.communicate(input=' '.join(INPUTS_FOR_CIN).
+                                                  encode('utf-8'))
+
+    stud_res = re.split(r'problem \d+\n', output_student.decode('utf-8').lower())
+    true_res = re.split(r'problem \d+\n', output_true.decode('utf-8').lower())
+
+    return true_res, stud_res
+
+
+trueResult, studentResult = compileCPP()
 
 
 @Test(f"Problem 1", 2)
 def is_valid(ag: Autograder, test: AutograderTest):
+    if COMPILED_STUDENT_SUBMISSION != 0 or COMPILED_TRUE_RESULT != 0:
+        test.print(f"There was an issue with compiling file")
+        return False
 
-	if COMPILED_STUDENT_SUBMISSION != 0 or COMPILED_TRUE_RESULT != 0:
-		test.print(f"There was an issue with compiling file")
-		return False
+    studSol = studentResult[1].strip()
+    trueSol = trueResult[1].strip()
 
-	studentResult = re.split(r'PROBLEM \d+\n', output_student.decode('utf-8'))
-	trueResult = re.split(r'PROBLEM \d+\n', output_true.decode('utf-8'))
-	is_correct = (studentResult[0] == trueResult[0])
+    is_correct = (trueSol in studSol and studSol != [''])
 
-	if is_correct == 0:
-		test.print("Your solution don't pass the test!")
-		return False
-	else:
-		test.print("Correct solution!")
-	return True
+    if is_correct == 0:
+        test.print("Your solution don't pass the test!")
+        return False
+    else:
+        test.print("Correct solution!")
+    return True
 
 
 @Test(f"Problem 2", 2)
 def is_valid(ag: Autograder, test: AutograderTest):
+    if COMPILED_STUDENT_SUBMISSION != 0 or COMPILED_TRUE_RESULT != 0:
+        test.print(f"There was an issue with compiling file")
+        return False
 
-	if COMPILED_STUDENT_SUBMISSION != 0 or COMPILED_TRUE_RESULT != 0:
-		test.print(f"There was an issue with compiling file")
-		return False
-
-	process = subprocess.Popen([f"./main"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	output_student, error_student = process.communicate(input=' '.join(INPUTS_FOR_CIN).encode('utf-8'))
-
-	process = subprocess.Popen([f"./truemain"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	output_true, error_true = process.communicate(input=' '.join(INPUTS_FOR_CIN).encode('utf-8'))
-
-	studentResult = re.split(r'PROBLEM \d+\n', output_student.decode('utf-8'))
-	trueResult = re.split(r'PROBLEM \d+\n', output_true.decode('utf-8'))
-	is_correct = (len(studentResult[1].split('\n')) == len(trueResult[1].split('\n')))
-
-	if is_correct == 0:
-		test.print("Your solution don't pass the test!")
-		return False
-	else:
-		test.print("Correct solution!")
-	return True
+    if applyPattern(studentResult, trueResult, 1) == 0:
+        test.print("Your solution don't pass the test!")
+        return False
+    else:
+        test.print("Correct solution!")
+    return True
 
 
 @Test(f"Problem 3", 2)
 def is_valid(ag: Autograder, test: AutograderTest):
+    if COMPILED_STUDENT_SUBMISSION != 0 or COMPILED_TRUE_RESULT != 0:
+        test.print(f"There was an issue with compiling file")
+        return False
 
-	if COMPILED_STUDENT_SUBMISSION != 0 or COMPILED_TRUE_RESULT != 0:
-		test.print(f"There was an issue with compiling file")
-		return False
-
-	process = subprocess.Popen([f"./main"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	output_student, error_student = process.communicate(input=' '.join(INPUTS_FOR_CIN).encode('utf-8'))
-
-	process = subprocess.Popen([f"./truemain"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	output_true, error_true = process.communicate(input=' '.join(INPUTS_FOR_CIN).encode('utf-8'))
-
-	studentResult = re.split(r'PROBLEM \d+\n', output_student.decode('utf-8'))
-	trueResult = re.split(r'PROBLEM \d+\n', output_true.decode('utf-8'))
-	is_correct = (studentResult[2] == trueResult[2])
-
-	if is_correct == 0:
-		test.print("Your solution don't pass the test!")
-		return False
-	else:
-		test.print("Correct solution!")
-	return True
+    if applyPattern(studentResult, trueResult, 3) == 0:
+        test.print("Your solution don't pass the test!")
+        return False
+    else:
+        test.print("Correct solution!")
+    return True
 
 
 @Test(f"Problem 4", 2)
 def is_valid(ag: Autograder, test: AutograderTest):
+    if COMPILED_STUDENT_SUBMISSION != 0 or COMPILED_TRUE_RESULT != 0:
+        test.print(f"There was an issue with compiling file")
+        return False
 
-	if COMPILED_STUDENT_SUBMISSION != 0 or COMPILED_TRUE_RESULT != 0:
-		test.print(f"There was an issue with compiling file")
-		return False
-
-	process = subprocess.Popen([f"./main"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	output_student, error_student = process.communicate(input=' '.join(INPUTS_FOR_CIN).encode('utf-8'))
-
-	process = subprocess.Popen([f"./truemain"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	output_true, error_true = process.communicate(input=' '.join(INPUTS_FOR_CIN).encode('utf-8'))
-
-	studentResult = re.split(r'PROBLEM \d+\n', output_student.decode('utf-8'))
-	trueResult = re.split(r'PROBLEM \d+\n', output_true.decode('utf-8'))
-	is_correct = (studentResult[3] == trueResult[3])
-
-	if is_correct == 0:
-		test.print("Your solution don't pass the test!")
-		return False
-	else:
-		test.print("Correct solution!")
-	return True
+    if applyPattern(studentResult, trueResult, 4) == 0:
+        test.print("Your solution don't pass the test!")
+        return False
+    else:
+        test.print("Correct solution!")
+    return True
 
 
 @Test(f"Problem 5", 2)
 def is_valid(ag: Autograder, test: AutograderTest):
+    if COMPILED_STUDENT_SUBMISSION != 0 or COMPILED_TRUE_RESULT != 0:
+        test.print(f"There was an issue with compiling file")
+        return False
 
-	if COMPILED_STUDENT_SUBMISSION != 0 or COMPILED_TRUE_RESULT != 0:
-		test.print(f"There was an issue with compiling file")
-		return False
-
-	process = subprocess.Popen([f"./main"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	output_student, error_student = process.communicate(input=' '.join(INPUTS_FOR_CIN).encode('utf-8'))
-
-	process = subprocess.Popen([f"./truemain"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	output_true, error_true = process.communicate(input=' '.join(INPUTS_FOR_CIN).encode('utf-8'))
-
-	studentResult = re.split(r'PROBLEM \d+\n', output_student.decode('utf-8'))
-	trueResult = re.split(r'PROBLEM \d+\n', output_true.decode('utf-8'))
-	is_correct = (studentResult[4] == trueResult[4])
-
-	if is_correct == 0:
-		test.print("Your solution don't pass the test!")
-		return False
-	else:
-		test.print("Correct solution!")
-	return True
+    if applyPattern(studentResult, trueResult, 5) == 0:
+        test.print("Your solution don't pass the test!")
+        return False
+    else:
+        test.print("Correct solution!")
+    return True
